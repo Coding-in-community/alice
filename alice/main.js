22057d0ece8229a1e7dbfdef187dd0e8e16f9b9c
BearingMe <gomes.bruno201@gmail.com>
2021-01-11 18:53:20 -0300

    [FEAT] added some new functions
    en: some new func were add for admins use
    
    pt: foram adicionadas algumas funções para administradores

--- alice/main.js
+++ alice/main.js
@@ -2,8 +2,12 @@ const Alice = require('./system')
 const { component } = require('./system/utils')
 
 let alice = new Alice([
-    component('banhammer', require('./scripts/kick')),
-    component('_example', require('./scripts/_example'))
+    component('add', require('./scripts/add')),
+    component('demote', require('./scripts/demote')),
+    component('github', require('./scripts/github')),
+    component('remove', require('./scripts/remove')),
+    component('link', require('./scripts/link')),
+    component('_example', require('./scripts/_example')),
 ])
 
 alice.init()
---
+++ alice/scripts/github.js
@@ -0,0 +1,3 @@
+module.exports = function(_, _, message) {
+    message.reply('https://github.com/Coding-in-community/whatsappbot-project_alice')
+}
