5121ad3fdeb58e802e9ed04cc181a0f2757c2c7f
BearingMe <gomes.bruno201@gmail.com>
2021-01-11 23:43:59 -0300

    [FIX] remove files and script for secury reasons
    en: all scripts that needs adm permission were disabled also
    session.json is no longer tracked by github
    
    pt: todos os scripts que necessitavam de permissão de adm foram
    desabilitados e o arquivo sessio.json não pode mais ser rastreado pelo
    github

--- alice/main.js
+++ alice/main.js
@@ -2,12 +2,12 @@ const Alice = require('./system')
 const { component } = require('./system/utils')
 
 let alice = new Alice([
-    component('add', require('./scripts/add')),
-    component('demote', require('./scripts/demote')),
     component('github', require('./scripts/github')),
-    component('remove', require('./scripts/remove')),
-    component('link', require('./scripts/link')),
     component('_example', require('./scripts/_example')),
+    // component('add', require('./scripts/add')),
+    // component('demote', require('./scripts/demote')),
+    // component('remove', require('./scripts/remove')),
+    // component('link', require('./scripts/link')),
 ])
 
 alice.init()
