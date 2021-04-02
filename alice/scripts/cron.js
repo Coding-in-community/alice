63280dc42147b570366e3183f6f86cca95d3a602
BearingMe <gomes.bruno201@gmail.com>
2021-04-02 01:16:26 -0300

    [REFACTOR] using classes instead functions

--- alice/scripts/cron.js
+++ alice/scripts/cron.js
@@ -1,89 +1,217 @@
 const events = require('events')
+const chattools = require('./utils/chattools')
+const time = require('./utils/time')
 
 let emitter = new events.EventEmitter()
 
-let time = require('./utils/time')
 
-let id = 0
-let idList = []
-
-module.exports = async function cron(data, message) {
-    let seconds = Number(data.kwargs.s) || 0
-    let minutes = Number(data.kwargs.m) || 0
-    let hours = Number(data.kwargs.h) || 0
-    let days = Number(data.kwargs.d) || 0
-    let timer = time.timer(seconds, minutes, hours, days)
-
-    if (data.args.includes('create')) {
-        if (timer > 0) {
-            id++
-            idList.push(id)
+function toPositiveNumber(value) {
+    let number = Number.parseFloat(value)
     
-            let interval
-            emitter.on('start_cron' + id, () => {
-                interval = setInterval(() => {
-                    message.reply(data.text)
-                }, timer)
+    if (number >= 0) {
+        return number
+    }
+    
+    else if (Number.isNaN(number)) {
+        return 0
+    }
+    
+    else {
+        return -number
+    }
+}
+
+
+let threads = [] 
+let counter = 0
+class Cron {
+    constructor(data, message) {
+        this.data = data
+        this.text = data.text
+        this.message = message
+
+        let seconds = toPositiveNumber(data.kwargs.s)
+        let minutes = toPositiveNumber(data.kwargs.m)
+        let hours = toPositiveNumber(data.kwargs.h)
+        let days = toPositiveNumber(data.kwargs.d)
+
+        this.timer = time.timer(seconds, minutes, hours, days)
+    }
+
+    _create() {         
+        // check time 
+        if (this.timer > 0) {
+            // id increment
+            counter++
+            
+            // create thread info
+            let thread = {}
+            thread._id = counter
+            thread.description = this.text.slice(0, 30) 
+            
+            // add event emitter  
+            let interval = null
+
+            // NÂO MEXER 
+            // DO NOT TOUCH 
+            // NE TOUCHEZ PAS
+            // NON TOCCARE
+            // 만지지 마십시오
+            // 触れないでください
+            let message = this.message 
+            let text = this.text
+            let timer = this.timer
+
+            thread.start = emitter.on('start-cron' + thread._id, function() {
+                interval = setInterval(() => message.reply(text), timer)
             })
-    
-            emitter.on('stop_cron' + id, () => {
+            
+            thread.stop = emitter.on('stop-cron' + thread._id, function() {
                 clearInterval(interval)
             })
     
-            message.reply(`thread created using id: ${id}`)
+            // save thread info
+            threads.push(thread)
+
+            return `thread created using id: ${thread._id}`
         }
 
-        else {
-            message.reply('you must add time')
+        return 'you must add a valid time' 
+    }
+    
+    _destroy() {
+        // check if is a valid id
+        if (threads.some(elem => (elem._id === Number(this.text)))) {
+            // call thread saved
+            let thread = threads.find(elem => elem._id === Number(this.text))
+            
+            // stop threads
+            this._stop()
+            
+            // remove threads emitters
+            emitter.removeAllListeners('start-cron' + this.text, thread.start)
+            emitter.removeAllListeners('stop-cron' + this.text, thread.stop)
+
+            // remove thread from array
+            threads = threads.filter(elem => !(elem._id === Number(this.text)))
+
+            return 'thread destroyed successfully'
         }
+
+        return 'thread not found'
+    }
+    
+    _start() {
+        // check if is a valid id
+        if (threads.some(elem => elem._id === Number(this.text))) {
+            emitter.emit('start-cron' + this.text)
+        
+            return `starting thread ${this.text}`
+        }
+        
+        return 'thread not found'
+    }
+    
+    _stop() {
+        // check if is a valid id
+        if (threads.some(elem => (elem._id === Number(this.text)))) {
+            emitter.emit('stop-cron' + this.text)
+            
+            return `stopping thread ${this.text}`
+        }
+        
+        return 'thread not found'
     }
 
-    if (data.args.includes('start')) {
-        if (idList.includes(Number(data.text))) {
-            emitter.emit('start_cron' + data.text)
-            message.reply(`starting thread ${data.text}`)
+    _log() {
+        let output = ''
+
+        if (threads.length === 0 )
+            output += 'thread not open'
+        else if (threads.length === 1)
+            output += 'thread open:\n\n'
+        else 
+            output += 'threads open:\n\n'
+
+        for (let thread of threads) {
+            console.log(thread)
+
+            let id = 'id: ' + thread._id + '\n'
+            let description = 'desc: ' + thread.description + '\n'
+
+            output += id + description + '\n'
         }
-        else {
-            message.reply('thread not found')
-        }
+
+        return output.trim()
     }
 
-    if (data.args.includes('stop')) {
-        if (idList.includes(Number(data.text))) {
-            emitter.emit('stop_cron' + data.text)
-            message.reply(`stopping thread ${data.text}`)
+    _killall() {
+        // loop for all threads
+        for (let thread in threads) {
+            
+
+            return 'all threads where killed'
         }
-        else {
-            message.reply('thread not found')
-        }
+
+        return 'something went wrong...'
     }
 
-    if (data.args.includes('destroy')) {
-        if (idList.includes(Number(data.text))) {
-            emitter.emit('stop_cron' + data.text)
-            message.reply(`stopping thread ${data.text}`)
+    _default() {
+        return `
+*criação*: _!cron --create --[time]=<int>_
+*outros*: _!cron [--flag] [<int>]_
 
-            emitter.off('start_cron' + data.text)
-            emitter.off('stop_cron' + data.text)
-            idList.splice(
-                idList.indexOf(data.text),
-                1
-            )
-
-            message.reply(`destroying thread ${data.text}`)
-        }
-
-        else {
-            message.reply('thread not found')
-        }
+*argumentos*:
+_--create -> cria uma nova thread_
+_--destroy -> apaga uma thread_
+_--start -> inicia uma thread_
+_--stop -> para uma thread_ 
+_--s -> define um intervalor de segundos_ 
+_--m -> define um intervalor de minutos_ 
+_--h -> define um intervalor de horas_ 
+_--d  -> define um intervalor de dias_ 
+        
+⚠️ *o uso indevido dessa função resultará em ban de 3 dias* ⚠️
+`.trim()
     }
 
-    if (data.args.includes('log')) {
-        let msg = 'the following services are open: '
-        msg += idList.join(', ')
+    code() {
+        if (this.data.args.includes('log')) 
+            return this._log()
+        
+        else if (this.data.args.includes('killall')) 
+            // return this._killall()
+            return 'sorry, this function is not done yet'
 
-        message.reply(msg)
+        else if (this.data.args.includes('create'))
+            return this._create()
+
+        else if (this.data.args.includes('destroy')) 
+            return this._destroy()
+
+        else if (this.data.args.includes('start'))
+            return this._start()
+
+        else if (this.data.args.includes('stop')) 
+            return this._stop()
+
+        else 
+            return this._default()
     }
+
+    main() {
+        if (chattools.isAdm(this.message)) {
+            return this.code()
+        }
+
+        return 'staff only'
+    }
+}
+
+module.exports = async function(data, message) {
+    let cron = new Cron(data, message)
+
+    return cron.main()
 }
 
 // create ==> automaticamente inicia
