0cd97bfcfc6321e4f6a82e0c7932b788bb94ba60
carlos3g <carlosmesquita156@gmail.com>
2021-07-15 20:35:31 -0300

    [REFACTOR] decreases number of if statements

--- alice/src/build/Components.js
+++ alice/src/build/Components.js
@@ -2,10 +2,6 @@ function isFunction(object) {
   return typeof object === 'function';
 }
 
-function isEmpty(object) {
-  return !object || Object.keys(object).length === 0;
-}
-
 class Components {
   constructor() {
     this.components = {};
@@ -16,24 +12,20 @@ class Components {
   }
 
   set(name, object) {
-    if (isFunction(object)) {
-      this.components[name] = object;
-    } else if (isEmpty(object)) {
-      throw new Error(`${name} component cannot be empty`);
-    } else {
+    if (!isFunction(object)) {
       throw new Error(`${object} must be a function`);
     }
+    this.components[name] = object;
   }
 
   async call(method, data, message, client) {
-    if (this.methods.includes(method)) {
-      const response = await this.components[method](data, message, client);
-
-      if (response) message.reply(String(response));
-    } else if (!this.methods.includes(method)) {
+    if (!this.methods.includes(method)) {
       throw new Error(`${method} is not registered`);
-    } else {
-      throw new Error('method call is not found');
+    }
+
+    const response = await this.components[method](data, message, client);
+    if (response) {
+      message.reply(String(response));
     }
   }
 }
