894c7ba4113e3b4b8d3bc635f387728a94214ecd
carlos3g <carlosmesquita156@gmail.com>
2021-07-17 15:26:43 -0300

    [REFACTOR] re-do lyric command

--- alice/scripts/lyric.js
+++ alice/scripts/lyric.js
@@ -3,50 +3,42 @@ const JSSoup = require('jssoup').default;
 const search = require('./utils/search');
 
 async function makeSoup(url) {
-  const response = await axios.get(url);
-  const { data: html } = response;
-
-  return new JSSoup(html);
+  const { data } = await axios.get(url);
+  return new JSSoup(data);
 }
 
-function removeBr(p) {
-  let html = p.prettify();
+function removeBr(raw) {
+  let html = raw.prettify();
 
   let htmlArray = html.split('\n');
-  htmlArray = htmlArray.filter((elem) => !elem.match(/<br[\s]*[\/]?>/));
-  htmlArray = htmlArray.map((elem) => elem.trim());
+  htmlArray = htmlArray.filter((e) => !e.match(/<br[\s]*[\/]?>/));
+  htmlArray = htmlArray.map((e) => e.trim());
   html = htmlArray.join('\n');
 
-  const soup = new JSSoup(html);
-  return soup;
+  return new JSSoup(html);
 }
 
 module.exports = async (data, message) => {
   const { text } = data;
 
-  if (text) {
-    const results = await search.google(text, 'https://www.letras.mus.br');
-
-    const { link: url } = results[0];
-
-    const soup = await makeSoup(url);
-
-    // titulo
-    const h1 = soup.find('div', { class: 'cnt-head_title' }).find('h1');
-
-    // letra
-    const div = soup.find('div', { class: 'cnt-letra' });
-    const pArray = div.findAll('p');
-
-    // monta a mensagem
-    let output = `*${h1.text}*\n`;
-    pArray.forEach((p) => {
-      output += removeBr(p).text;
-    });
-    output += `\n_${url}_`;
-
-    message.reply(output);
-  } else {
+  if (!text) {
     message.reply('please tell me the song name');
   }
+
+  const results = await search.google(text, 'https://www.letras.mus.br');
+  const { link } = results[0];
+  const soup = await makeSoup(link);
+
+  const title = soup.find('div', { class: 'cnt-head_title' }).find('h1');
+  const lyricsDiv = soup.find('div', { class: 'cnt-letra' });
+  const pArray = lyricsDiv.findAll('p');
+
+  const output = `\
+*${title.text}*
+
+${pArray.map((p) => removeBr(p).text).join('')}
+
+_${url}_`;
+
+  message.reply(output);
 };
