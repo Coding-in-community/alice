28c3ec681a35613fbc250ba364c6969b9963d0bd
carlos3g <carlosmesquita156@gmail.com>
2021-07-16 21:50:31 -0300

    [REFACTOR] re-do suggest command

--- alice/scripts/suggest.js
+++ alice/scripts/suggest.js
@@ -1,48 +1,35 @@
 const parse = require('./utils/parse');
-const random = require('./utils/random');
 
-module.exports = (data, message, client) => {
-  const defaultMessage = `
-uso: _!suggest [--flag] [descrição]_
+const myID = parse.userID('+55 11 96734-3809');
+const defaultMessage = `
+uso: _!suggest [--flag] [sugestão]_
 
 argumentos:
-  _--feature  algo novo que deseja que criemos_
-  _--change  o que deve ser mudado_ 
-  _--remove  aquilo que devemos remover_ 
+  _--feature  para sugerir algo novo_
+  _--change   para sugerir mudanças_ 
+  _--remove   para sugerir remoções_ 
 
-⚠️ *o uso indevido dessa função resultará em ban de 3 dias* ⚠️
-    `;
+⚠️ *o uso indevido dessa função resultará em ban de 3 dias* ⚠️`;
 
-  const responses = [
-    'sua solicitação será analisada e discutida pela administração',
-    'veremos o que podemos fazer',
-    'obrigado pela colaboração',
-    'discutiremos assim que possivel',
-    'obrigado. chame novamente caso queira continuar a contribuir',
-  ];
+module.exports = (data, msg, client) => {
+  const { args, text } = data;
 
-  const myID = parse.userID('+55 11 96734-3809');
+  const reportMsg = `⚠️ *${args[0]} suggestion* ⚠️\n\n${text}`;
 
-  if (data.args.length === 0 && data.text) {
-    return 'nenhuma flag foi fornecida';
+  if (args.length === 0 && text) {
+    return 'Nenhuma flag foi fornecida.';
   }
-  if (data.args.length > 0 && !data.text) {
-    return 'nenhuma descrição foi fornecida';
+  if (args.length > 0 && !text) {
+    return 'Nenhuma sugestão foi fornecida.';
   }
-  if (data.args.includes('feature')) {
-    const text = `⚠️ *feature suggestion* ⚠️\n\n${data.text}`;
-    client.sendMessage(myID, text);
-    return random.choice(responses);
-  }
-  if (data.args.includes('change')) {
-    const text = `⚠️ *change suggestion* ⚠️\n\n${data.text}`;
-    client.sendMessage(myID, text);
-    return random.choice(responses);
-  }
-  if (data.args.includes('remove')) {
-    const text = `⚠️ *remove suggestion* ⚠️\n\n${data.text}`;
-    client.sendMessage(myID, text);
-    return random.choice(responses);
+
+  if (
+    args.includes('feature') ||
+    args.includes('remove') ||
+    args.includes('change')
+  ) {
+    client.sendMessage(myID, reportMsg);
+    return 'obrigado pela colaboração';
   }
   return defaultMessage.trim();
 };
