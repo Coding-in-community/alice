9a569ffea58b46b7b7527a45eaa9e926d1cae4a9
carlos3g <carlosmesquita156@gmail.com>
2021-07-19 03:14:34 -0300

    [REFACTOR] refactor parse and time

--- alice/scripts/utils/parse.js
+++ alice/scripts/utils/parse.js
@@ -1,18 +1,12 @@
-const assert = require('assert');
-
 function userID(targetNumber) {
-  assert.strictEqual(
-    typeof targetNumber,
-    'string',
-    'you must pass the number as a string'
-  );
+  if (typeof targetNumber !== 'string') {
+    throw new Error('you must pass the number as a string');
+  }
 
   const target = targetNumber.replace(/\D/g, '');
-
   const regexp = /\d+/;
   const matches = target.match(regexp);
   const pattern = matches[0];
-
   return `${pattern}@c.us`;
 }
 
--- alice/scripts/utils/time.js
+++ alice/scripts/utils/time.js
@@ -2,37 +2,13 @@ function sleep(seconds) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
-function timer(sec, min = 0, hour = 0, day = 0) {
-  // segundos
-  if (sec >= 0) {
-    sec *= 1000;
-  } else {
-    throw new Error('seconds must be higher than 0');
-  }
-
-  // minutos
-  if (min >= 0) {
-    min = min * 60 * 1000;
-  } else {
-    throw new Error('minutes must be higher than 0');
-  }
-
-  // horas
-  if (hour >= 0) {
-    hour = hour * 60 * 60 * 1000;
-  } else {
-    throw new Error('hours must be higher than 0');
-  }
-
-  // day
-  if (day >= 0) {
-    day = day * 24 * 60 * 60 * 100;
-  } else {
-    throw new Error('minutes must be higher than 0');
-  }
-
-  const time = sec + min + hour + day;
-  return time;
+function timer(sec = 0, min = 0, hour = 0, day = 0) {
+  const secsInMS = sec * 1000;
+  const minsInMS = min * 60 * 1000;
+  const hoursInMS = hour * 60 * 60 * 1000;
+  const daysInMS = day * 24 * 60 * 60 * 1000;
+  const timeInMS = secsInMS + minsInMS + hoursInMS + daysInMS;
+  return timeInMS;
 }
 
 module.exports = {
