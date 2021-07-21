40285510521962ae29d31c0e0205bf39f5250918
carlos3g <carlosmesquita156@gmail.com>
2021-07-21 17:49:21 -0300

    [DOCS] documents some files in the 'auth' folder

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,11 +1,17 @@
-const whatsapp = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const path = require('path');
 
 const FILE_NAME = 'session.json';
 
-class Session extends whatsapp.Client {
+/**
+ * Starting point for interacting with the WhatsApp Web API.
+ * @param {string} SESSION_FILE_PATH - Path to `session.json` file.
+ * @see https://docs.wwebjs.dev/Client.html
+ * @extends {Client}
+ */
+class Session extends Client {
   constructor() {
     super({
       puppeteer: {
@@ -16,10 +22,17 @@ class Session extends whatsapp.Client {
     this.SESSION_FILE_PATH = path.join(__dirname, FILE_NAME);
   }
 
+  /**
+   * Checks if the `session.json` file already exists.
+   * @returns {boolean} - `True` if exists, `False` if not.
+   */
   get exists() {
     return fs.existsSync(this.SESSION_FILE_PATH);
   }
 
+  /**
+   * Throws the QR-Code to authenticate the session. When the QR-Code is read, the session file is written.
+   */
   create() {
     this.on('qr', (qr) => {
       qrcode.generate(qr, { small: true });
@@ -27,11 +40,18 @@ class Session extends whatsapp.Client {
     this.on('authenticated', this.save);
   }
 
+  /**
+   * Writes the session in a .json file (`this.SESSION_FILE_PATH`)
+   * @param {object} session - The session file returned in the authentication.
+   */
   save(session) {
     fs.writeFileSync(this.SESSION_FILE_PATH, JSON.stringify(session));
     console.log('⚠ The current session has been saved ⚠');
   }
 
+  /**
+   * Loads the saved session file.
+   */
   load() {
     if (!this.exists) {
       throw Error(`session data does not exist in ${this.SESSION_FILE_PATH}`);
@@ -44,6 +64,9 @@ class Session extends whatsapp.Client {
     console.log('⚠ The previous session was loaded ⚠');
   }
 
+  /**
+   * Starts the session.
+   */
   start() {
     this.on('ready', () => {
       console.log('Client is ready!');
