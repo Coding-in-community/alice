2c4c36ce5f7f0a81a5096efcc6f9c0afc4580edc
carlos3g <carlosmesquita156@gmail.com>
2021-07-21 17:49:45 -0300

    [DOCS] documents some files in the 'utils' folder

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,6 +1,13 @@
 const googleIt = require('google-it');
 
-async function google(query, target = '', limit = null) {
+/**
+ * Searchs for a given string in google.
+ * @param {string} query Text that must be searched.
+ * @param {string} [target=''] Target site to search in.
+ * @param {number} [limit=0] Max number of results that must be returned.
+ * @returns {object[]} Array of results found.
+ */
+async function google(query, target = '', limit = 0) {
   const result = await googleIt({
     query,
     includeSites: target,
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,12 +1,25 @@
+/**
+ * Suspends (waits) execution of the current thread for a given number of seconds.
+ * @param {number} seconds
+ * @returns {Promise}
+ */
 function sleep(seconds) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
-function timer(sec = 0, min = 0, hour = 0, day = 0) {
-  const secsInMS = sec * 1000;
-  const minsInMS = min * 60 * 1000;
-  const hoursInMS = hour * 60 * 60 * 1000;
-  const daysInMS = day * 24 * 60 * 60 * 1000;
+/**
+ * Converts a given set of seconds, minutes, hours and days in miliseconds.
+ * @param {number} [secs=0]
+ * @param {number} [mins=0]
+ * @param {number} [hours=0]
+ * @param {number} [days=0]
+ * @returns {number} Total time in miliseconds.
+ */
+function timer(secs = 0, mins = 0, hours = 0, days = 0) {
+  const secsInMS = secs * 1000;
+  const minsInMS = mins * 60 * 1000;
+  const hoursInMS = hours * 60 * 60 * 1000;
+  const daysInMS = days * 24 * 60 * 60 * 1000;
   const timeInMS = secsInMS + minsInMS + hoursInMS + daysInMS;
   return timeInMS;
 }
