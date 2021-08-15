b17457cf60955873dd1c4f0afd52afe6f38e8104
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-08-15 01:08:35 -0300

    [FEAT] adds includesOnly and excludes options to inhibitor

--- src/utils/inhibitor.js
+++ src/utils/inhibitor.js
@@ -1,16 +1,18 @@
 const chattools = require('./chattools');
 
 /**
- * For a given message that called an command/module and its options, checks if the command/module should be inhibited.
+ * For a given message that called an command/module and its options, checks if the command/module must be inhibited.
  * @see https://docs.wwebjs.dev/Message.html
  * @param {object} options
+ * @param {string[]} options.excludes
  * @param {boolean} options.isAdmOnly
+ * @param {string[]} options.includesOnly
  * @param {string[]} options.scope
  * @param {Message} message
- * @returns {boolean} `true` if the command/module should be inhibited, `false` if not.
+ * @returns {boolean} `true` if the command/module must be inhibited, `false` if not.
  */
 async function inhibitor(options, message) {
-  const { isAdmOnly, scope = [] } = options;
+  const { isAdmOnly, scope = [], includesOnly = [], excludes = [] } = options;
   const chat = await message.getChat();
 
   if (
@@ -21,6 +23,22 @@ async function inhibitor(options, message) {
   }
 
   if (isAdmOnly && !(await chattools.isFromAdm(message))) {
+    return true;
+  }
+
+  if (
+    includesOnly.length > 0 &&
+    // eslint-disable-next-line no-underscore-dangle
+    includesOnly.every((i) => i !== chat.id._serialized)
+  ) {
+    return true;
+  }
+
+  if (
+    excludes.length > 0 &&
+    // eslint-disable-next-line no-underscore-dangle
+    excludes.some((id) => id === chat.id._serialized)
+  ) {
     return true;
   }
 
