90d524a6c33b3bc7a2609b28566bed5be4b9cb54
carlos3g <52337966+carlos3g@users.noreply.github.com>
2021-08-15 18:23:03 -0300

    [REFACTOR] uses defaultOptions to prevent 'destructuring a undefined'

--- src/utils/inhibitor.js
+++ src/utils/inhibitor.js
@@ -1,5 +1,12 @@
 const chattools = require('./chattools');
 
+const defaultOptions = {
+  excludes: [],
+  isAdmOnly: false,
+  includesOnly: [],
+  scope: ['group', 'private_chat'],
+};
+
 /**
  * For a given message that called an command/module and its options, checks if the command/module must be inhibited.
  * @see https://docs.wwebjs.dev/Message.html
@@ -12,8 +19,11 @@ const chattools = require('./chattools');
  * @returns {boolean} `true` if the command/module must be inhibited, `false` if not.
  */
 async function inhibitor(options, message) {
-  const { isAdmOnly, scope = [], includesOnly = [], excludes = [] } = options;
   const chat = await message.getChat();
+  const { isAdmOnly, scope, includesOnly, excludes } = {
+    ...defaultOptions,
+    ...options,
+  };
 
   if (
     (chat.isGroup && !scope.includes('group')) ||
