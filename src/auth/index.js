07a568affd6fa7c04b74b7d66bcf046b0f320444
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2022-03-01 22:03:28 -0300

    [FEAT] implements multidevice support

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
 const path = require('path');
 const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
 /**
  * Starting point for interacting with the WhatsApp Web API.
@@ -11,65 +10,19 @@ const { Client } = require('whatsapp-web.js');
 class Session extends Client {
   constructor() {
     super({
+      authStrategy: new LocalAuth({
+        dataPath: path.join(__dirname, 'sessions'),
+      }),
       puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       },
     });
-
-    this.SESSION_FILE_PATH = path.join(__dirname, 'session.json');
   }
 
-  /**
-   * Checks if the `session.json` file exists.
-   * @returns {boolean} - `true` if exists, `false` if not.
-   */
-  get exists() {
-    return fs.existsSync(this.SESSION_FILE_PATH);
-  }
-
-  /**
-   * Throws the QR-Code to authenticate the session. When the QR-Code is read, the session file is written.
-   * @private
-   */
-  create() {
+  start() {
     this.on('qr', (qr) => {
       qrcode.generate(qr, { small: true });
     });
-    this.on('authenticated', this.save);
-  }
-
-  /**
-   * Writes the session in a .json file (`this.SESSION_FILE_PATH`)
-   * @private
-   * @param {object} session - The session object.
-   */
-  save(session) {
-    fs.writeFileSync(this.SESSION_FILE_PATH, JSON.stringify(session));
-  }
-
-  /**
-   * Loads the saved session file.
-   * @private
-   */
-  load() {
-    if (!this.exists) {
-      throw Error(`session data does not exist in ${this.SESSION_FILE_PATH}`);
-    }
-
-    const raw = fs.readFileSync(this.SESSION_FILE_PATH);
-    const data = JSON.parse(raw);
-    this.options.session = data;
-  }
-
-  /**
-   * Starts the session (Creates it if doesn't exists).
-   */
-  start() {
-    if (this.exists) {
-      this.load();
-    } else {
-      this.create();
-    }
 
     this.on('ready', () => {
       console.log('Client is ready!');
